989d099be8aaebe3880b1535fdd21b637c87d665
Tenzin Lhakhang <2402007+tlhakhan@users.noreply.github.com>
2022-12-24 20:37:05 -0500

    Use enable_ flag to turn on and off specific sub-modules

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     helm = {
-      source  = "hashicorp/helm"
+      source = "hashicorp/helm"
     }
     kubernetes = {
       source = "hashicorp/kubernetes"
@@ -10,138 +10,74 @@ terraform {
 }
 
 #
-# Metal LB
+# MetalLB add-on
 #
-resource "helm_release" "metallb" {
-  name             = "metallb"
-  repository       = "https://metallb.github.io/metallb"
-  chart            = "metallb"
-  version          = "0.13.7"
-  namespace        = "metallb-system"
-  create_namespace = true
+variable "enable_metallb" {
+  type        = bool
+  description = "Enable MetalLB add-on"
+  default     = false
 }
 
-resource "helm_release" "metallb_config" {
-  depends_on = [helm_release.metallb]
-  name       = "metallb-config"
-  chart      = "${path.module}/metallb-config-chart"
+variable "metallb_params" {
+  type = object({
+    ip_pool_range = string
+  })
 
-  set {
-    name  = "metallb_ip_pool_range"
-    value = var.metallb_ip_pool_range
+  default = {
+    ip_pool_range = "10.0.0.1-10.0.0.10"
   }
-
 }
 
-#
-# NFS subdir provisioner
-#
-resource "helm_release" "nfs_subdir" {
-  name             = "nfs-subdir"
-  repository       = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner"
-  chart            = "nfs-subdir-external-provisioner"
-  version          = "4.0.16"
-  namespace        = "storage-system"
-  create_namespace = true
-
-  set {
-    name  = "nfs.server"
-    value = var.nfs_subdir.server
-  }
-
-  set {
-    name  = "nfs.path"
-    value = var.nfs_subdir.path
-  }
-
-  set {
-    name  = "nfs.mountOptions"
-    value = "{nodiratime,noatime,nfsvers=3,nconnect=16,noacl,nolock}"
-  }
-
-  set {
-    name  = "storageClass.defaultClass"
-    value = true
-  }
+module "metallb" {
+  count         = var.enable_metallb ? 1 : 0
+  source        = "./modules/metallb"
+  ip_pool_range = var.metallb_params.ip_pool_range
 }
 
 #
-# Splunk
+# NFS subdir provisioner add-on
 #
-resource "helm_release" "splunk_operator" {
-  name             = "splunk-operator"
-  chart            = "splunk-operator"
-  repository       = "https://splunk.github.io/splunk-operator"
-  version          = "1.0.0"
-  namespace        = "monitoring"
-  create_namespace = true
+variable "enable_nfs_subdir" {
+  type        = bool
+  description = "Enable the NFS subdir provisioner add-on"
+  default     = false
 }
 
-resource "helm_release" "splunk_enterprise" {
-  depends_on = [helm_release.splunk_operator]
-  name       = "splunk-enterprise"
-  chart      = "splunk-enterprise"
-  repository = "https://splunk.github.io/splunk-operator"
-  version    = "1.0.0"
-  namespace  = "monitoring"
-  skip_crds  = true
-
-  set {
-    name = "sva.s1.enabled"
-    value = true
+variable "nfs_subdir_params" {
+  type = object({
+    server              = string
+    path                = string
+    mountOptions        = string
+    defaultStorageClass = bool
+  })
+  default = {
+    server              = "nfsserver-1"
+    path                = "/vmgr"
+    mountOptions        = "nfsvers=3"
+    defaultStorageClass = true
   }
-  set {
-    name  = "splunk-operator.enabled"
-    value = false
-  }
-
-  set {
-    name  = "clusterMaster.enabled"
-    value = true
-  }
-
-  set {
-    name  = "indexerCluster.enabled"
-    value = true
-  }
-
-  set {
-    name  = "searchHeadCluster.enabled"
-    value = true
-  }
-
 }
 
-data "kubernetes_secret" "splunk_secrets" {
-  depends_on = [helm_release.splunk_enterprise]
-  metadata {
-    name = "splunk-stdln-standalone-secret-v1"
-    namespace = "monitoring"
-  }
+module "nfs_subdir" {
+  count               = var.enable_nfs_subdir ? 1 : 0
+  source              = "./modules/nfs-subdir"
+  server              = var.nfs_subdir_params.server
+  path                = var.nfs_subdir_params.path
+  mountOptions        = var.nfs_subdir_params.mountOptions
+  defaultStorageClass = var.nfs_subdir_params.defaultStorageClass
 }
 
 #
-# Fluent Bit
+# Monitoring add-on
+# Adds Splunk, Fluent Bit, Prometheus, Grafana
 #
-resource "helm_release" "fluent_bit" {
-  depends_on = [helm_release.splunk_operator]
-  name = "fluent-bit"
-  chart = "fluent-bit"
-  repository = "https://fluent.github.io/helm-charts"
-  version = "0.21.6"
-  namespace = "monitoring"
-  set {
-    name = "config.outputs"
-    value = "${data.template_file.fluent_bit_config_outputs.rendered}"
-  }
+variable "enable_monitoring" {
+  type        = bool
+  description = "Enable the Monitoring add-on"
+  default     = false
 }
 
-data "template_file" "fluent_bit_config_outputs" {
-  template = file("${path.module}/fluent-bit/config.outputs.ini")
-  vars = {
-    splunk_hec_token = "${data.kubernetes_secret.splunk_secrets.data.hec_token}"
-    splunk_host = "splunk-stdln-standalone-service"
-    splunk_hec_port = "8088"
-  }
+module "monitoring" {
+  count  = var.enable_monitoring ? 1 : 0
+  source = "./modules/monitoring"
 }
-
